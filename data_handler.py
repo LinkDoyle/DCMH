a281dcebf3cb6a69a65a15786800d6be5410bc7d
Link <linkdoyle@163.com>
2020-06-23 14:20:01 +0800

    Add execution time record

--- data_handler.py
+++ data_handler.py
@@ -1,15 +1,15 @@
 import h5py
 import scipy.io as scio
 import numpy as np
+import time
 
 def load_data(path):
-    file = h5py.File(path, mode='r')
-    images = file['images'][:].astype('float')
-    labels = file['LAll'][:]
-    tags = file['YAll'][:]
-
-    file.close()
-
+    start_time = time.time()
+    with h5py.File(path, mode='r') as file:
+        images = file['images'][:].astype('float')
+        labels = file['LAll'][:]
+        tags = file['YAll'][:]
+    print(f'loaded data {time.time() - start_time:.4f}s')
     labels = np.transpose(labels)
     tags = np.transpose(tags)
     
